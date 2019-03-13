1efe389942ede5ff9c667a6ebb11ee5d9e6da736
MilanJanjic <milanjanjic23@yahoo.com>
2019-03-13 18:30:41 +0100

    Updates

---
+++ precache-manifest.86c2c3949df32bd66ee28a2a7191f43e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f7c3e0b3a260ba501d2",
+    "url": "/react-knjiga/static/js/runtime~main.6f80847a.js"
+  },
+  {
+    "revision": "6a85daf9a2fafdf10796",
+    "url": "/react-knjiga/static/js/main.910650e0.chunk.js"
+  },
+  {
+    "revision": "afe8afdb0c633e251577",
+    "url": "/react-knjiga/static/js/2.479927cb.chunk.js"
+  },
+  {
+    "revision": "6a85daf9a2fafdf10796",
+    "url": "/react-knjiga/static/css/main.54f0cbcc.chunk.css"
+  },
+  {
+    "revision": "3d3e1fc85fe3cfddbafbb146ceefe3c4",
+    "url": "/react-knjiga/index.html"
+  }
+];
